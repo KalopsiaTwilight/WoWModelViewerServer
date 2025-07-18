efc458ab5fbb02607dbb952a3f774d49a7623229
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2025-07-18 20:44:10 +0200

    Revert JSON serialization to camelCase

--- Server/Program.cs
+++ Server/Program.cs
@@ -15,11 +15,7 @@ namespace Server
 
             // Add services to the container.
 
-            builder.Services.AddControllers()
-                .AddJsonOptions((opt) =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                });
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
