758a12d2f1de44e9e4f5296ed08f81d29adfaffa
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2025-10-28 09:26:55 +0100

    Simplify tactsharp provider

--- ModelViewer.Core/Providers/TACTSharpFileDataProvider.cs
+++ ModelViewer.Core/Providers/TACTSharpFileDataProvider.cs
@@ -20,6 +20,7 @@ namespace ModelViewer.Core.Providers
             _buildInstance.Settings.Product = productName;
             _buildInstance.Settings.Locale = RootInstance.LocaleFlags.enUS;
             _buildInstance.Settings.RootMode = RootInstance.LoadMode.Full;
+            _buildInstance.Settings.TryCDN = false;
 
             var baseFolder = config["CASC:BasePath"];
             if (string.IsNullOrEmpty(baseFolder))
@@ -27,22 +28,7 @@ namespace ModelViewer.Core.Providers
                 throw new InvalidOperationException("No WoW path set under CASC:BasePath. Invalid configuration.");
             }
 
-            var buildInfoPath = Path.Combine(baseFolder, ".build.info");
-            if (!File.Exists(buildInfoPath))
-                throw new InvalidOperationException("No build.info found in base directory");
-
-
             _buildInstance.Settings.BaseDir = baseFolder;
-            var buildInfo = new BuildInfo(buildInfoPath, _buildInstance.Settings, _buildInstance.cdn);
-
-            var build = buildInfo.Entries.First(x => x.Product == productName);
-            _buildInstance.Settings.BuildConfig = build.BuildConfig;
-            _buildInstance.Settings.CDNConfig = build.CDNConfig;
-            if (!string.IsNullOrEmpty(build.Armadillo))
-            {
-                _buildInstance.cdn.ArmadilloKeyName = build.Armadillo;
-            }
-
 
             // Load encryption keys;
             if (!File.Exists("WoW.txt"))
@@ -54,6 +40,19 @@ namespace ModelViewer.Core.Providers
                     var outStream = File.OpenWrite("WoW.txt");
                     stream.CopyTo(outStream);
                 }).Wait();
+            }
+
+            var buildInfoPath = Path.Combine(baseFolder, ".build.info");
+            if (!File.Exists(buildInfoPath))
+                throw new InvalidOperationException("No build.info found in base directory");
+            var buildInfo = new BuildInfo(buildInfoPath, _buildInstance.Settings, _buildInstance.cdn);
+
+            var build = buildInfo.Entries.First(x => x.Product == productName);
+            _buildInstance.Settings.BuildConfig = build.BuildConfig;
+            _buildInstance.Settings.CDNConfig = build.CDNConfig;
+            if (!string.IsNullOrEmpty(build.Armadillo))
+            {
+                _buildInstance.cdn.ArmadilloKeyName = build.Armadillo;
             }
 
             _buildInstance.cdn.OpenLocal();
@@ -68,33 +67,7 @@ namespace ModelViewer.Core.Providers
 
         public Stream GetFileById(uint filedataId)
         {
-            var rootEntries = _buildInstance.Root!.GetEntriesByFDID(filedataId);
-            if (rootEntries.Count == 0) {
-                throw new FileNotFoundException();
-            }
-
-            var preferredEntry = rootEntries.FirstOrDefault(x =>
-                !x.contentFlags.HasFlag(RootInstance.ContentFlags.LowViolence) &&
-                (x.localeFlags.HasFlag(_buildInstance.Settings.Locale) || x.localeFlags.HasFlag(RootInstance.LocaleFlags.All_WoW)
-            ));
-
-            if (preferredEntry.fileDataID == 0)
-            {
-                preferredEntry = rootEntries.First();
-            }
-            var fileEKeys = _buildInstance.Encoding!.FindContentKey(preferredEntry.md5);
-            if (!fileEKeys)
-            {
-                throw new Exception("EKey not found in encoding.");
-            }
-
-            var eKey = fileEKeys[0];
-            var (offset, size, archiveIndex) = _buildInstance.GroupIndex!.GetIndexInfo(eKey);
-            byte[] fileBytes;
-            if (offset == -1)
-                fileBytes = _buildInstance.cdn.GetFile("data", Convert.ToHexStringLower(eKey), 0, fileEKeys.DecodedFileSize, true);
-            else
-                fileBytes = _buildInstance.cdn.GetFileFromArchive(Convert.ToHexStringLower(eKey), _buildInstance.CDNConfig!.Values["archives"][archiveIndex], offset, size, fileEKeys.DecodedFileSize, true);
+            var fileBytes = _buildInstance.OpenFileByFDID(filedataId);
             return new MemoryStream(fileBytes);
         }
     }
